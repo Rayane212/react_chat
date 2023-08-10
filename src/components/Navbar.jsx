6d2eb3baa33147552a2b743786d1c559f1e204df
Rayane Hadi <rayanehadi41@gmail.com>
2023-08-10 23:43:42 +0200

    maj

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,20 @@ import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 import { AuthContext } from '../context/AuthContext';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import MenuIcon from '@mui/icons-material/Menu';
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 
 const Navbar = () => {
-    const {currentUser} = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
     return (
         <div className='navbar'>
-            <div className='logoMenu'>
-                <MenuIcon className="menu" />
+            <div className='logoMsg'>
+                <QuestionAnswerIcon className="msgIcon" />
                 <span className='logo'>React Chat</span>
             </div>
             <div className="user">
                 <img src={currentUser.photoURL} alt="" />
                 <span>{currentUser.displayName}</span>
-                <PowerSettingsNewIcon className='logout'  onClick={()=>signOut(auth)}/>
+                <PowerSettingsNewIcon className='logout' onClick={() => signOut(auth)} />
             </div>
         </div>
     );
