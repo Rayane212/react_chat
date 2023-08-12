487fd77ec7fe06e563ab97b2d66d13bc7b68f461
Rayane Hadi <rayanehadi41@gmail.com>
2023-08-12 14:33:29 +0200

    Alert for auth error of firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase'
 import { useNavigate, Link } from 'react-router-dom'
+import { Alert } from '@mui/material';
 
 const Login = () => {
-    const [err, setErr] = useState(false);
+    const [err, setErr] = useState("");
     const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
@@ -16,10 +17,17 @@ const Login = () => {
             await signInWithEmailAndPassword(auth, email, password);
             navigate("/");
 
-        } catch (err) {
-            setErr(true);
+        } catch (error) {
+            const errorCode = error.code;
+            if (errorCode === "auth/user-not-found") {
+                setErr("User not found");
+            } else if (errorCode === "auth/wrong-password") {
+                setErr("Wrong password");
+            } else {
+                setErr("Something went wrong");
+            }
+            
         }
-
     }
 
     return (
@@ -28,10 +36,10 @@ const Login = () => {
                 <span className='logo'>React Chat</span>
                 <span className='title'>Login</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="email" name="" id="" />
-                    <input type="password" placeholder="password" name="" id="" />
+                    <input type="email" placeholder="email" name="" id="" required />
+                    <input type="password" placeholder="password" name="" id="" required />
                     <button>Sign in</button>
-                    {err && <span>Something went wrong</span>}
+                    {err !== "" && <Alert className="" severity="error" >{err}</Alert>}
                 </form>
                 <p>You don't have an account? <Link to="/register">Register</Link> </p>
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,89 +1,89 @@
 import React, { useState } from 'react';
-import Add from '../img/addAvatar.png'
+import Add from '../img/addAvatar.png';
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth, storage, db } from "../firebase"
+import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { doc, setDoc } from "firebase/firestore"
-import { useNavigate, Link } from 'react-router-dom'
-
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate, Link } from 'react-router-dom';
+import { Alert } from '@mui/material';
 
 const Register = () => {
-    const [err, setErr] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate()
+  const [err, setErr] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        setLoading(true);
-        e.preventDefault();
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    setErr("")
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
-        try {
-            const res = await createUserWithEmailAndPassword(auth, email, password);
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `img/avatars/${displayName + date}`);
 
-            const date = new Date().getTime();
-            const storageRef = ref(storage, `img/avatars/${displayName + date}` );
+      await uploadBytesResumable(storageRef, file).then(async () => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
 
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
 
-            await uploadBytesResumable(storageRef, file).then(() => {
-                getDownloadURL(storageRef).then(async (downloadURL) => {
-                  try {
-                    //Update profile
-                    await updateProfile(res.user, {
-                      displayName,
-                      photoURL: downloadURL,
-                    });
-                    //create user on firestore
-                    await setDoc(doc(db, "users", res.user.uid), {
-                      uid: res.user.uid,
-                      displayName,
-                      email,
-                      photoURL: downloadURL,
-                    });
-        
-                    //create empty user chats on firestore
-                    await setDoc(doc(db, "userChats", res.user.uid), {});
-                    navigate("/");
-                  } catch (err) {
-                    console.log(err);
-                    setErr(true);
-                    setLoading(false);
-                  }
-                });
-              });
-            } catch (err) {
-              setErr(true);
-              setLoading(false);
-            }
-          };
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            navigate("/");
+          } catch (err) {
+            setErr("Something went wrong");
+          } finally {
+            setLoading(false);
+          }
+        });
+      });
+    } catch (error) {
+      const errorCode = error.code;
+      if (errorCode === "auth/email-already-in-use") {
+        setErr("Email already exists. Please use a different email.");
+      } else {
+        setErr("Something went wrong");
+      }
+      setLoading(false);
+    }
+  };
 
-    
-
-return (
+  return (
     <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className='logo'>React Chat</span>
-            <span className='title'>Register</span>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder='display name' />
-                <input type="email" placeholder="email" name="" id="email" />
-                <input type="password" placeholder="password" name="" id="password" />
-                <input style={{ display: "none" }} type="file" name="" id="file" />
-                <label htmlFor='file'>
-                    <img src={Add} alt="" />
-                    <span>Add an Avatar</span>
-                </label>
-                <button>Sign up</button>
-                {loading && "Uploading and compressing the image please wait..."}
-                {err && <span>Something went wrong</span>}
-            </form>
-            <p>You do have an account? <Link to="/login">Login</Link> </p>
+      <div className='formWrapper'>
+        <span className='logo'>React Chat</span>
+        <span className='title'>Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder='display name' />
+          <input type="email" placeholder="email" id="email" required />
+          <input type="password" placeholder="password" id="password" required />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor='file'>
+            <img src={Add} alt="" />
+            <span>Add an Avatar</span>
+          </label>
+          <button>Sign up</button>
+          {loading && <Alert severity="info">Uploading and compressing the image, please wait...</Alert>}
+          {err && <Alert severity="error">{err}</Alert>}
 
-        </div>
+        </form>
+        <p>You already have an account? <Link to="/login">Login</Link> </p>
+      </div>
     </div>
-);
+  );
 };
 
 export default Register;
