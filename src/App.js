a4163f97ac3193ae58bc81c11f000f54de041f10
Rayane Hadi <rayanehadi41@gmail.com>
2023-08-15 11:45:08 +0200

    mobile

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import Login from './pages/Login';
 import './style.scss';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from './context/AuthContext';
 
 function App() {
   const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const enterFullscreen = () => {
+      const element = document.documentElement;
+
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    };
+
+    if (window.screen.orientation) {
+      window.screen.orientation.lock('portrait').catch(() => {
+        console.warn("L'orientation en mode portrait n'a pas pu être verrouillée.");
+      });
+    }
+
+    enterFullscreen();
+  }, []);
 
   const ProtectedRoute = ({ children }) => {
     if (!currentUser) {
