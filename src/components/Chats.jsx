802422c11ec78f2aaf4e04dc69675d7fed6089b2
Rayane HADI <r.hadi@dailybiz.com>
2023-08-17 18:01:11 +0200

    styled badge

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -6,7 +6,7 @@ import { ChatContext } from '../context/ChatContext';
 import { Avatar, Badge, Tooltip } from '@mui/material';
 import { signOut } from 'firebase/auth';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 
 const Chats = () => {
     const [chats, setChats] = useState([]);
@@ -86,6 +86,35 @@ const Chats = () => {
         }
     };
 
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+          backgroundColor: '#44b700',
+          color: '#44b700',
+          boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+          '&::after': {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            borderRadius: '50%',
+            animation: 'ripple 1.2s infinite ease-in-out',
+            border: '1px solid currentColor',
+            content: '""',
+          },
+        },
+        '@keyframes ripple': {
+          '0%': {
+            transform: 'scale(.8)',
+            opacity: 1,
+          },
+          '100%': {
+            transform: 'scale(2.4)',
+            opacity: 0,
+          },
+        },
+      }));
+
     
 
 
@@ -100,19 +129,18 @@ const Chats = () => {
                             onClick={() => handleSelect(chatId, chat.userInfo)}
                         >
                             <>
-                                <Badge
-                                    badgeContent=""
-                                    color="error"
+                                <StyledBadge
                                     overlap="circular"
+                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
                                     variant="dot"
-                                    invisible={!chat.lastMessage?.unread}
+                                    invisible={!chat.userInfo.online}
                                 >
                                     <Avatar
                                         className='img'
                                         src={chat.userInfo.photoURL}
                                         alt={chat.userInfo.displayName}
                                     />
-                                </Badge>
+                                </StyledBadge>
                             </>
 
 
