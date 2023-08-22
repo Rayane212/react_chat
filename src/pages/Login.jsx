c7b853a454b6b7c1356755afcdb2c75bbd782c46
Rayane HADI <r.hadi@dailybiz.com>
2023-08-22 17:03:22 +0200

    SignInWithGoogle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,70 @@
 import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth, db } from '../firebase'
+import { linkWithPopup, linkWithRedirect, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { auth, db, googleProvider } from '../firebase'
 import { useNavigate, Link } from 'react-router-dom'
-import { Alert } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { collection, doc, updateDoc } from 'firebase/firestore';
+import { Alert, IconButton } from '@mui/material';
+import { FacebookOutlined, Visibility, VisibilityOff } from '@mui/icons-material';
+import { collection, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import GoogleIcon from '@mui/icons-material/Google';
 
 const Login = () => {
     const [err, setErr] = useState("");
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate()
 
+    const handleSignInWithGoogle = async () => {
+        try {
+            const res = await signInWithPopup(auth, googleProvider);
+
+            const userRef = doc(db, "users", res.user.uid);
+            const userSnap = await getDoc(userRef);
+
+            if (userSnap.exists() && !userSnap.data().googleLinked) {
+                await linkGoogleAccount(res.user.uid);
+            } else if (!userSnap.exists()) {
+                await createNewUser(res.user);
+            }
+
+            navigate("/");
+
+        } catch (error) {
+            console.error("Error signing in with Google:", error);
+        }
+    };
+
+    const linkGoogleAccount = async (uid) => {
+        try {
+            await linkWithRedirect(auth.currentUser, googleProvider);
+            await updateDoc(doc(db, "users", uid), { googleLinked: true });
+
+            const userDoc = await getDoc(doc(db, "users", uid));
+            const userData = userDoc.data();
+
+            // Update photoURL only if it's not set from before
+            if (!userData.photoURL && auth.currentUser.photoURL) {
+                await updateDoc(doc(db, "users", uid), { photoURL: auth.currentUser.photoURL });
+            }
+        } catch (error) {
+            console.error("Error linking Google account:", error);
+        }
+    };
+
+    const createNewUser = async (user) => {
+        try {
+            const { uid, displayName, email, photoURL } = user;
+            await setDoc(doc(db, "users", uid), {
+                uid,
+                displayName,
+                email,
+                photoURL,
+                googleLinked: true,
+                online: true,
+            });
+            await setDoc(doc(db, "userChats", uid), {});
+        } catch (error) {
+            console.error("Error creating new user:", error);
+        }
+    };
     const handleSubmit = async (e) => {
         e.preventDefault();
         const email = e.target[0].value;
@@ -29,6 +83,7 @@ const Login = () => {
             if (errorCode === "auth/user-not-found") {
                 setErr("User not found");
             } else if (errorCode === "auth/wrong-password") {
+                console.error(error)
                 setErr("Wrong password");
             } else {
                 setErr("Something went wrong");
@@ -53,12 +108,22 @@ const Login = () => {
                             {showPassword ? <Visibility /> : <VisibilityOff />}
                         </span>
                     </div>
-
-
+                    <div style={{margin:'0 auto'}}>
+                    <IconButton sx={{width:'fit-content', marginRight:'5px'}} onClick={handleSignInWithGoogle}>
+                        <GoogleIcon/>
+                    </IconButton>
+                    <IconButton sx={{width:'fit-content', marginLeft:'5px'}} onClick={handleSignInWithGoogle}>
+                        <FacebookOutlined/>
+                    </IconButton>
+                    </div>
                     <button>Sign in</button>
                     {err !== "" && <Alert className="" severity="error" >{err}</Alert>}
                 </form>
+                <div style={{textAlign:"center"}}>
+                     {/* <p>Forgot your password ?</p> */}
                 <p>You don't have an account? <Link to="/register">Register</Link> </p>
+                </div>
+               
 
             </div>
         </div>
