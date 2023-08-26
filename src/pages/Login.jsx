95c6bfcba340a15b504d0e2646213cc09e78769d
Rayane Hadi <rayanehadi41@gmail.com>
2023-08-26 03:12:18 +0200

    maj login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,49 +11,6 @@ const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate()
 
-    // const handleSignInWithGoogle = async () => {
-    //     try {
-    //         const res = await signInWithPopup(auth, googleProvider);
-
-    //         const userRef = doc(db, "users", res.user.email);
-    //         const userSnap = await getDoc(userRef);
-
-    //         console.log(userSnap)
-
-    //         if (!userSnap.googleLinked) {
-    //             await createNewUser(res.user);
-    //         }
-
-    //         navigate("/");
-
-    //     } catch (error) {
-    //         console.error("Error signing in with Google:", error);
-    //     }
-    // };
-
-
-
-    // const createNewUser = async (user) => {
-    //     try {
-    //         const { uid } = user;
-    //         await updateDoc(doc(db, "users", uid), {
-    //             googleLinked: true,
-    //             online: true,
-    //         });
-    //         const userChats = doc(db, "userChats", uid)
-    //         console.log(getDoc(userChats))
-    //         if (getDoc(userChats) === null) {
-    //             await setDoc(doc(db, "userChats", uid), {});
-    //         }
-
-    //     } catch (error) {
-    //         console.error("Error creating new user:", error);
-    //     }
-    // };
-
-
-
-
     const handleSubmit = async (e) => {
         e.preventDefault();
         const email = e.target[0].value;
@@ -72,7 +29,6 @@ const Login = () => {
             if (errorCode === "auth/user-not-found") {
                 setErr("User not found");
             } else if (errorCode === "auth/wrong-password") {
-                console.error(error)
                 setErr("Wrong password");
             } else {
                 setErr("Something went wrong");
@@ -97,23 +53,12 @@ const Login = () => {
                             {showPassword ? <Visibility /> : <VisibilityOff />}
                         </span>
                     </div>
-                    {/* <div style={{ margin: '0 auto' }}>
-                        <IconButton sx={{ width: 'fit-content', marginRight: '5px' }} onClick={handleSignInWithGoogle}>
-                            <GoogleIcon />
-                        </IconButton>
-                        <IconButton sx={{ width: 'fit-content', marginLeft: '5px' }} onClick={handleSignInWithGoogle}>
-                            <FacebookOutlined />
-                        </IconButton>
-                    </div> */}
+
+
                     <button>Sign in</button>
                     {err !== "" && <Alert className="" severity="error" >{err}</Alert>}
-
                 </form>
-                <div style={{ textAlign: "center" }}>
-                    {/* <p>Forgot your password ?</p> */}
-                    <p>You don't have an account? <Link to="/register">Register</Link> </p>
-                </div>
-
+                <p>You don't have an account? <Link to="/register">Register</Link> </p>
 
             </div>
         </div>
