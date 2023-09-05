4fa2bef4b35551ecf546ddda171e1780af624584
Rayane <86132260+Rayane212@users.noreply.github.com>
2023-09-05 15:03:47 +0000

    maj

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,6 +9,7 @@ export const AuthContextProvider = ({ children }) => {
 
     useEffect(() => {
         const unsub = onAuthStateChanged(auth, (user) => {
+            // set Current user
             setCurrentUser(user);
         });
 
