b6988beb0cfe8dcb64271c40d73e74fd386accb9
Rayane <86132260+Rayane212@users.noreply.github.com>
2023-09-05 08:15:54 +0000

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import AuthContext from './context/AuthContext'; 
 import App from './App';
 
-const authContextValue = {
-  currentUser: null, 
-};
 
 test('rendre App.js sans erreur', () => {
   render(
     <BrowserRouter>
-      <AuthContext.Provider value={authContextValue}>
         <App />
-      </AuthContext.Provider>
     </BrowserRouter>
   );
 
