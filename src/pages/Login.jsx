926594054fc4fbe61cb1fd2a30983191764d81ca
Rayane Hadi <rayanehadi41@gmail.com>
2023-09-07 00:24:29 +0200

    Test Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,12 +9,13 @@ import { collection, doc, updateDoc } from 'firebase/firestore';
 const Login = () => {
     const [err, setErr] = useState("");
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const email = e.target[0].value;
-        const password = e.target[1].value;
 
         try {
             await signInWithEmailAndPassword(auth, email, password);
@@ -43,9 +44,9 @@ const Login = () => {
                 <span className='logo'>React Chat</span>
                 <span className='title'>Login</span>
                 <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="email" name="email" id="email" autoComplete="username" required />
+                    <input type="email" placeholder="email" name="email" id="email" autoComplete="username" onChange={(e) => setEmail(e.target.value)} value={email} required/>
                     <div className="passwordInput">
-                        <input type={showPassword ? "text" : "password"} placeholder="password" name="password" id="password" autoComplete="current-password" required />
+                        <input type={showPassword ? "text" : "password"} placeholder="password" name="password" id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} value={password} required />
                         <span
                             className="passwordIcon"
                             onClick={() => setShowPassword(!showPassword)}
---
+++ src/tests/Login.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import Login from '../pages/Login';
+import { MemoryRouter } from 'react-router-dom';
+
+
+describe('Login page', () => {
+    it('renders without crashing', () => {
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+        expect(screen.getByText('Login')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('email')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('password')).toBeInTheDocument();
+    });
+    it('handles form submission with valid data', async () => {
+
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+        const emailInput = screen.getByPlaceholderText('email');
+        const passwordInput = screen.getByPlaceholderText('password');
+
+        fireEvent.change(emailInput, { target: { value: 'rayanehadi41@gmail.com' } });
+        fireEvent.change(passwordInput, { target: { value: 'azerty1234' } });
+
+        const submitButton = screen.getByText('Sign in');
+        fireEvent.click(submitButton);
+
+
+    });
+});
