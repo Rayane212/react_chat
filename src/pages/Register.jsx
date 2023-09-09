1a6b582b3871fbaf6733a64d273af02ec39f2d1f
Rayane Hadi <rayanehadi41@gmail.com>
2023-09-10 01:08:47 +0200

    bug fix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Add from '../img/addAvatar.png';
-import { Loading } from '../components/mui/Loading'
+import Loading from '../components/mui/Loading'
 import { createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
@@ -92,55 +92,57 @@ const Register = () => {
   };
 
   return (
-    <div className='formContainer'>
-      <div className='formWrapper'>
-        <span className='logo'>React Chat</span>
-        <span className='title'>Register</span>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Username' onChange={(e) => setDisplayName(e.target.value)} required />
-          <input type="email" placeholder="Email" id="email" onChange={(e) => setEmail(e.target.value)} required />
-          <input type="tel" placeholder="+33612536545" id="tel" onChange={(e) => setPhoneNumber(e.target.value)} required />
-          <div className="passwordInput">
-            <input type={showPassword ? "text" : "password"} placeholder="Password" name="password" id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} required />
-            <span
-              className="passwordIcon"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? <Visibility /> : <VisibilityOff />}
-            </span>
-          </div>
-          <input style={{ display: "none" }} type="file" id="file" accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
-          <label htmlFor='file'>
-            {imagePreview ? (
-              <>
-                <img src={Add} alt="" className='addPhoto' />
-                <Avatar className="avatarPreview" src={imagePreview} alt='Avatar' />
-                <span> preview your avatar </span>
-                <Tooltip title="Clear">
-                  <Clear onClick={(e) => {
-                    e.preventDefault()
-                    setFile("");
-                    setImagePreview("")
-                  }} />
-                </Tooltip>
-              </>
+    <>
+      <div className='formContainer'>
+        <div className='formWrapper'>
+          <span className='logo'>React Chat</span>
+          <span className='title'>Register</span>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Username' onChange={(e) => setDisplayName(e.target.value)} required />
+            <input type="email" placeholder="Email" id="email" onChange={(e) => setEmail(e.target.value)} required />
+            <input type="tel" placeholder="+33612536545" id="tel" onChange={(e) => setPhoneNumber(e.target.value)} required />
+            <div className="passwordInput">
+              <input type={showPassword ? "text" : "password"} placeholder="Password" name="password" id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} required />
+              <span
+                className="passwordIcon"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </span>
+            </div>
+            <input style={{ display: "none" }} type="file" id="file" accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+            <label htmlFor='file'>
+              {imagePreview ? (
+                <>
+                  <img src={Add} alt="" className='addPhoto' />
+                  <Avatar className="avatarPreview" src={imagePreview} alt='Avatar' />
+                  <span> preview your avatar </span>
+                  <Tooltip title="Clear">
+                    <Clear onClick={(e) => {
+                      e.preventDefault()
+                      setFile("");
+                      setImagePreview("")
+                    }} />
+                  </Tooltip>
+                </>
 
-            ) : (
-              <>
-                <img src={Add} alt="" className='img' />
-                <span>Add an Avatar</span>
-              </>
+              ) : (
+                <>
+                  <img src={Add} alt="" className='img' />
+                  <span>Add an Avatar</span>
+                </>
 
-            )}
-          </label>
-          <button>Sign up</button>
-          {loading && <Loading loading={loading} />}
-          {err && <Alert severity="error" sx={{ width: "250px" }}>{err}</Alert>}
+              )}
+            </label>
+            <button>Sign up</button>
+            {err && <Alert severity="error" sx={{ width: "250px" }}>{err}</Alert>}
 
-        </form>
-        <p>You already have an account? <Link to="/login">Login</Link> </p>
+          </form>
+          <p>You already have an account? <Link to="/login">Login</Link> </p>
+        </div>
       </div>
-    </div>
+      {loading && <Loading loading={loading} />}
+    </>
   );
 };
 
